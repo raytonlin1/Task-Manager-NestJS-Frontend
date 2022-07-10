baa3232435f0cd82fdd9b45f6e0bcbab3d27f83f
raytonlin1 <raytonlin1@users.noreply.github.com>
2022-07-10 15:28:48 +0000

    deploy: 3d596fd5e51e4ef95654072736283309dc646d75

---
+++ precache-manifest.911ed601054365291a701a41dee10e1b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71d89b05526e9756fd9c228a8b2825b6",
+    "url": "https://raytonlin1.github.io/task-client/index.html"
+  },
+  {
+    "revision": "2b27a1df52569ddd1412",
+    "url": "https://raytonlin1.github.io/task-client/static/css/main.84a53bfd.chunk.css"
+  },
+  {
+    "revision": "8080778d80b100af1fca",
+    "url": "https://raytonlin1.github.io/task-client/static/js/2.46bdf46b.chunk.js"
+  },
+  {
+    "revision": "2b27a1df52569ddd1412",
+    "url": "https://raytonlin1.github.io/task-client/static/js/main.6353cd3d.chunk.js"
+  },
+  {
+    "revision": "04c4d6729d171a452b47",
+    "url": "https://raytonlin1.github.io/task-client/static/js/runtime~main.2c48ed7f.js"
+  }
+]);
